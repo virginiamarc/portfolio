5579e1d1d1d4347f022e33cdc50f5c51c65d3c91
Virginia Marc <vmarc4@hccfl.edu>
2026-06-01 22:02:45 -0400

    Update alt for banner image

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -7,7 +7,7 @@ const RightBanner = () => {
         <img 
             className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
             src={bannerImg} 
-            alt="Banner Image" 
+            alt="Virginia Marc" 
         />
         <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r 
             from-[#1E1E20] to-[#2C2C30] shadow-shadowOne flex justify-center items-center rounded-full filter"
