72d50c816f117362c64c9dc6801413368394bb6c
Virginia Marc <vmarc4@hccfl.edu>
2026-06-09 01:52:00 -0400

    Updated banner image

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React from "react";
 import { bannerImg } from "../../assets/images";
 
 const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative">
-        <img 
-            className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
-            src={bannerImg} 
-            alt="Virginia Marc" 
+
+      {/* Circle Glow Container */}
+      <div
+        className="relative flex justify-center items-center
+        w-[400px] h-[400px]
+        md:w-[480px] md:h-[480px]
+        lgl:w-[650px] lgl:h-[650px]
+        bg-gradient-to-br from-[#1E1E20] to-[#2C2C30]
+        rounded-full shadow-shadowOne overflow-hidden"
+      >
+        {/* Image INSIDE the circle */}
+        <img
+          className="w-[300px] h-[420px]
+          md:w-[360px] md:h-[540px]
+          lgl:w-[500px] lgl:h-[720px]
+          object-cover"
+          src={bannerImg}
+          alt="Virginia Marc"
         />
-        <div className="absolute bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r 
-            from-[#1E1E20] to-[#2C2C30] shadow-shadowOne flex justify-center items-center rounded-full filter"
-        ></div>
+      </div>
+
     </div>
   );
-}
+};
 
 export default RightBanner;
